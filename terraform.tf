28be820408d5444e6d0912d945b9f14350c8205a
Jesse Young <jesse@jajit.com>
2023-12-21 12:54:37 -0500

    Remove cloud block.

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "VizaLogix"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
